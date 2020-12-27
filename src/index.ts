3c3199a4643be80e3cff987ec2e5f736dcba8319
Kevin Wade <kevinrwade@gmail.com>
2020-12-26 18:36:25 -0600

    Add basic fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+import Fastify from "fastify";
+
+// Require the framework and instantiate it
+const fastify = Fastify({ logger: true });
+
+// Declare a route
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+// Run the server!
+const port = 3000;
+const start = async () => {
+  try {
+    await fastify.listen(port);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
